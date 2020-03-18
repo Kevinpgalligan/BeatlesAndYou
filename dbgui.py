0f61bc49a451e687391e3d1aad7c47efd95de2de
kevinpgalligan <galligankevinp@gmail.com>
2020-03-18 23:43:36 +0000

    Fix bug when no lyrics entered; sort songs; copy search query to clipboard.

--- dbgui.py
+++ dbgui.py
@@ -1,12 +1,15 @@
 import PySimpleGUI as sg
+import pyperclip
 from storage import LyricsDatabase
 
 COLUMNS = [
+    "id",
     "name",
     "artist"
 ]
 
 def create_window(header, data):
+    done = sum(1 if cell[-1] else 0 for cell in data)
     layout = [
         [
             sg.Table(
@@ -17,12 +20,14 @@ def create_window(header, data):
                 justification="right",
                 enable_events=True,
                 num_rows=min(len(data), 10),
-                row_colors=[(i, "Green" if data[i][2] else "Red")
+                row_colors=[(i, "Green" if data[i][-1] else "Red")
                             for i in range(len(data))],
                 key="TABLE")
         ],
         [sg.Text(
-            "Coverage: {}%".format(round(sum(1 if cell[2] else 0 for cell in data)/len(data)*100., 2)))]
+            "Coverage: {}%; {} remaining.".format(
+                round(done/len(data)*100., 2),
+                len(data)-done))]
     ]
     return sg.Window("Table", layout, grab_anywhere=False)
 
@@ -30,11 +35,12 @@ def main():
     db = LyricsDatabase("./lyrics/charts/")
     header = COLUMNS + ["has-lyrics"]
     data = []
-    songs = []
+    song_by_id = {}
     for song in db:
-        songs.append(song)
+        song_by_id[song["id"]] = song
         data.append(
             [song[field] for field in COLUMNS] + [True if "lyrics" in song else False])
+    data.sort(key=lambda cell: cell[-1])
     sg.theme("Black")
     window = create_window(header, data)
     while True:
@@ -43,12 +49,14 @@ def main():
             break
         if event == "TABLE":
             row_index = values["TABLE"][0]
-            song = songs[row_index]
-            lyrics = sg.PopupGetText("Input lyrics of {}".format(song["name"])).strip()
+            song = song_by_id[data[row_index][0]]
+            pyperclip.copy(song["artist"] + " " + song["name"] + " " + "lyrics")
+            lyrics = sg.PopupGetText("Input lyrics of {}".format(song["name"]))
             if lyrics is not None and len(lyrics) > 0:
-                song["lyrics"] = lyrics
+                song["lyrics"] = lyrics.strip()
                 db.write(song)
-                data[row_index][2] = True
+                data[row_index][-1] = True
+                data.sort(key=lambda cell: cell[-1])
                 window.close()
                 window = create_window(header, data)
     window.close()
